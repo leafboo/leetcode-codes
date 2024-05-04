bacd07307d8386285ef9699fa86dbabd41adc2ca
Jan <110758056+leafboo@users.noreply.github.com>
2024-05-04 16:05:29 +0800

    Time: 189 ms (5.05%), Space: 59.2 MB (45.58%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -1,16 +1,25 @@
 function isPalindrome(x: number): boolean {
-    const stringNumber = x.toString();
-
+     // Convert the integer to a string
+    const str = x.toString();
+    
+    // Initialize pointers for the start and end of the string
     let left = 0;
-    let right = stringNumber.length - 1;
-
-    while(left < right) {
-        if (stringNumber[left] !== stringNumber[right]) {
-            return false
+    let right = str.length - 1;
+    
+    // Continue checking until the pointers meet
+    while (left < right) {
+        // If characters at left and right pointers are not equal, it's not a palindrome
+        if (str[left] !== str[right]) {
+            return false;
         }
+        // Move the pointers towards the center
         left++;
         right--;
     }
-
+    
+    // If the loop completes, the string is a palindrome
     return true;
 };
+// 12345
+// 1221
+// 11
