b7b7189050eae309d4b131264672356a9a1fab48
Jan <110758056+leafboo@users.noreply.github.com>
2024-05-10 17:53:42 +0800

    Time: 53 ms (83.74%), Space: 13.1 MB (54.55%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -5,13 +5,12 @@ class Solution(object):
         :rtype: int
         """
         l = 0
-        r = 1
-        while r != len(nums):
+        for r in range(1, len(nums)):
             if nums[l] == nums[r]:
-                nums.pop(r)
-                
+                r += 1 
             else:
+                nums[l + 1] = nums[r]
                 l += 1
-                r += 1
+                
         return l + 1
             
